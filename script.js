e2581ebf643c965e260586258386220f61355e29
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-10-18 11:10:00 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         AdBlock YouTube - block pop-up display
+// @description  Script de desactivation de la pop up YouTube sur l'utilisation d'AdBlock
+// @author       HugoTby
+// @match        https://www.youtube.com/
+// @icon         https://example.com/favicon.ico
+// @version      0.0.1
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    // Ajoute du CSS personnalisé
+    GM_addStyle(`
+        /* Votre CSS personnalisé ici */
+        .style-scope ytd-app {
+            display: none;
+        }
+    `);
+})();
